95a07a60e4e050dd398ec4322e02aeb749378c7c
ElisabethKAndersson <>
2023-06-23 22:39:05 +0000

    Set up board look

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def build_board(size):
+    return [['~' for count in range(size)] for count in range(size)]
+build_board(8)
+
+def print_board(board):
+    for b in board:
+        print(*b)
+board = build_board(4)
+print_board(board)
