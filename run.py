4c4118fc65a8c0285fe0c5aef81816c3a40f1526
ElisabethKAndersson <>
2023-06-24 18:58:59 +0000

    Add welcome text and a board for the opponent

--- run.py
+++ run.py
@@ -1,9 +1,27 @@
+
 def build_board(size):
     return [['~'] * 8 for x in range(8)]
 
 
-def print_board(board):
+def player_board(board):
     for b in board:
         print(*b)
+
+def npc_board(board):
+    for b in board:
+        print(*b)
+
 board = build_board(4)
-print_board(board)
+print("Your board")
+player_board(board)
+print("\nOpponents board")
+npc_board(board)
+
+
+
+def run_game():
+    print("WELCOME TO BATTLESHIP")
+    print("An enemy is spotted on the horizon")
+    print("Try to sink all their ships before they sink your ships")
+    
+run_game()
