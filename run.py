26a93b9605688b987c515bbdb29d1b5772a08176
ElisabethKAndersson <>
2023-06-25 18:58:14 +0000

    add some ships, same on both bords for now

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
+from random import randint
+
 """
 Builds basic board layout.
 """
 def build_board(size):
-    return [['~'] * 8 for x in range(8)]
-
+    return [['~'for x in range(8)] for y in range (8)]
 
 def player_board(board):
     for b in board:
         print(*b)
+  
 
 def npc_board(board):
     for b in board:
@@ -16,7 +18,12 @@ def npc_board(board):
 board = build_board(4)
 
 
-
+def create_ships(npc_board):
+    for ship in range(6):
+        ship_r, ship_cl=randint(0,7), randint(0,7)
+        while board[ship_r][ship_cl] =='@':
+            ship_r, ship_cl = randint(0, 7), randint(0, 7)
+        board[ship_r][ship_cl] = '@'
 
 def run_game():
     print("\nWELCOME TO BATTLESHIP")
@@ -24,9 +31,10 @@ def run_game():
     print("Try to sink all their ships before they sink your ships.\n")
 
 
+
 run_game()
 print("Your board")
+create_ships(npc_board)
 player_board(board)
 print("\nOpponents board")
 npc_board(board)
-print(create_ships)
