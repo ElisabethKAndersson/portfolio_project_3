fc827b2ad3de1d951897320207cd3ad5da59b8ab
ElisabethKAndersson <>
2023-06-25 22:36:17 +0000

    Add input function for ship placement guess

--- run.py
+++ run.py
@@ -17,6 +17,9 @@ def npc_board(board):
 
 board = build_board(4)
 
+"""
+Put random ships on the board. Code from. "https://copyassignment.com/battleship-game-code-in-python/"
+"""
 
 def create_ships(npc_board):
     for ship in range(6):
@@ -25,16 +28,22 @@ def create_ships(npc_board):
             ship_r, ship_cl = randint(0, 7), randint(0, 7)
         board[ship_r][ship_cl] = '@'
 
+
+#Start-text
+
 def run_game():
     print("\nWELCOME TO BATTLESHIP")
     print("\nAn enemy fleet is spotted on the horizon.")
     print("Try to sink all their ships before they sink your ships.\n")
 
-
-
+def guess_place():
+    row = input("Write a row number 0-7:")
+    col = input("Write a column number 0-7:")
+   
 run_game()
 print("Your board")
 create_ships(npc_board)
 player_board(board)
 print("\nOpponents board")
 npc_board(board)
+guess_place()
