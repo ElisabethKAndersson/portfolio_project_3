5b027e27bb82eda0849a3efa17d04fc582f4951c
ElisabethKAndersson <>
2023-07-09 20:08:34 +0000

    Add code to place ships and to guess placement

--- run copy.py
+++ run copy.py
@@ -1,48 +1,99 @@
 from random import randint
 
-"""
-Builds basic board layout.
-"""
-def build_board(size):
-    return [['~'for x in range(8)] for y in range (8)]
+class Battleship: 
+    
+    #Global variables for starting up game.
+    
+    def __init__(self): 
+        self.user_board = []
+        self.computer_board = []
+        self.user_hits = 0
+        self.computer_hits = 0
+        self.ships = 5
+          
 
-def player_board(board):
-    for b in board:
-        print(*b) 
+    def start(self):
 
-def npc_board(board):
-    for b in board:
-        print(*b)
+        #Function to create both boards and place ships.
 
-board = build_board(4)
-
-"""
-Put random ships on the board. Code from. "https://copyassignment.com/battleship-game-code-in-python/"
-"""
-
-def create_ships(npc_board):
-    for ship in range(6):
-        ship_r, ship_cl=randint(0,7), randint(0,7)
-        while board[ship_r][ship_cl] =='@':
-            ship_r, ship_cl = randint(0, 7), randint(0, 7)
-        board[ship_r][ship_cl] = '@'
+        self.user_board = self.create_boards()
+        self.computer_board = self.create_boards()
+        self.display_boards()
+        self.user_board  = self.place_ships(self.user_board)
+        self.computer_board = self.place_ships(self.computer_board)
+        #self.guess_place = []
 
 
-#Start-text
+    def create_boards(self):
 
-def run_game():
-    print("\nWELCOME TO BATTLESHIP")
-    print("\nAn enemy fleet is spotted on the horizon.")
-    print("Try to sink all their ships before they sink your ships.\n")
+        # Board layout to be used in both
+                
+        num = 8
 
-def guess_place():
-    row = input("Write a row number 0-7:")
-    col = input("Write a column number 0-7:")
-   
-run_game()
-print("Your board")
-create_ships(npc_board)
-player_board(board)
-print("\nOpponents board")
-npc_board(board)
-guess_place()
+        ls = [['~'for x in range(num)] for y in range (num)]
+        some_str = ''
+        for i in range (len(ls)):
+            some_str+=" ".join(ls[i])+'\n'
+        return(some_str)
+
+    
+    def place_ships(num):
+        
+            """
+        This function generates a random placement of ships on a battleship game board.
+    
+        Parameters:
+        num (int): The size of the game board
+    
+        Returns:
+        str: A string representation of the game board with ship placements
+        """
+        try:
+            # Check if the input is a positive integer
+            if not isinstance(num, int) or num <= 0:
+                raise ValueError("Input must be a positive integer")
+        
+            # Create an empty game board
+            ls = [['~' for x in range(num)] for y in range(num)]
+            
+            # Place random ships on the game board
+            for i in range(num):
+                x = random.randint(0, num-1)
+                y = random.randint(0, num-1)
+                ls[x][y] = 'S'
+            
+            # Generate a string representation of the game board
+            some_str = ''
+            for i in range(len(ls)):
+                some_str += " ".join(ls[i]) + '\n'
+            
+            return some_str
+        except ValueError as e:
+            # Log the error
+            print(f"Error: {e}")
+            return ''
+        
+            
+    """
+     def guess_place(self.computer_board):
+        x = input("Write a row number 0-7:\n")
+        y = input("Write a column number 0-7:\n")
+    
+    """
+
+    def display_boards(self):
+        print("\nWELCOME TO BATTLESHIP")
+        print("\nAn enemy fleet is spotted on the horizon.")
+        print("Try to sink all their ships before they sink your ships.\n")
+        print("Your board:")
+        print(self.user_board)
+        print("Computer board:")
+        print(self.computer_board)
+
+        self.place_ships(self.computer_board)
+        self.place_ships(self.user_board)
+        self.guess_place(self.computer_board)
+        
+battleship = Battleship()
+# start the game
+battleship.start()
--- run.py
+++ run.py
@@ -9,43 +9,56 @@ class Battleship:
         self.computer_board = []
         self.user_hits = 0
         self.computer_hits = 0
-        self.ships = []
+        self.ships = 5
+          
 
-    def start(self, num_ships):
+    def start(self):
 
         #Function to create both boards and place ships.
 
         self.user_board = self.create_boards()
         self.computer_board = self.create_boards()
         self.display_boards()
-        self.num_ships = num_ships
         self.user_board  = self.place_ships(self.user_board)
         self.computer_board = self.place_ships(self.computer_board)
+        #self.guess_place = []
 
 
     def create_boards(self):
 
         # Board layout to be used in both
+                
+        num = 8
 
-        ls = [['~'for x in range(8)] for y in range (8)]
+        ls = [['~'for x in range(num)] for y in range (num)]
         some_str = ''
         for i in range (len(ls)):
             some_str+=" ".join(ls[i])+'\n'
         return(some_str)
 
-    def place_ships(self, board):
+    
+    def place_ships(self, user_board):
 
+        num_ships = 0
+        while num_ships < self.ships:
+            x = random.randint(0, 7)
+            y = random.randint(0, 7)
+            if board[x][y] == '~':
+                board[x][y] = '@'
+                num_ships += 1
+        
 
-        """
-        for ship in range(6):
-            ship_r, ship_cl=randint(0,7), randint(0,7)
-        while board[ship_r][ship_cl] =='@':
-            ship_r, ship_cl = randint(0, 7), randint(0, 7)
-        board[ship_r][ship_cl] = '@'
-
-        return board
-        """
-
+   
+    def guess_place(self, computer_board):
+        x = int(input("Write a row number 0-7:\n"))
+        y = int(input("Write a column number 0-7:\n"))
+        if board[x][y] == '@':
+            print("Hit!")
+            self.user_hits += 1
+        else:
+            print("Miss!")
+    
+  
 
     def display_boards(self):
         print("\nWELCOME TO BATTLESHIP")
@@ -56,7 +69,10 @@ class Battleship:
         print("Computer board:")
         print(self.computer_board)
 
-
+        self.place_ships(self.computer_board)
+        self.place_ships(self.user_board)
+        self.guess_place(self.computer_board)
+        
 battleship = Battleship()
 # start the game
 battleship.start()
