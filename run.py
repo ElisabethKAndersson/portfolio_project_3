e11e2bca94658653601be52cfce9425f8670e723
ElisabethKAndersson <>
2023-07-22 18:31:06 +0000

    Add guess ship location input function

--- run.py
+++ run.py
@@ -14,6 +14,16 @@ def create_board(board):
         space+=" ".join(board[i])+'\n'
     return(space)
 
+def guess_ship_location():
+    row = input("Please enter a row number 0-7:")
+    while row not in "01234567":
+        print('That is not a valid row number')
+        row = input("Please enter a row number 0-7:")
+    column = input("Write a column number 0-7:")
+    while column not in "01234567":
+        print('That is not a valid column number')
+        column = input("Write a column number 0-7:")
+
 
 print("\nWELCOME TO BATTLESHIP")
 print("\nAn enemy fleet is spotted on the horizon.")
@@ -22,4 +32,5 @@ print("Try to sink all their ships before it's too late.\n")
 time.sleep(5)
 print("\nCan you guess where on the board the 6 hidden ships are?")
 print(create_board(Guess_Pattern))
+guess_ship_location()
     
