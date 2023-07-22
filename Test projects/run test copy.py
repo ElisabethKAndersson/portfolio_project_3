8ee5c2fd8b2e080140e150669136c9969962cad6
ElisabethKAndersson <>
2023-07-22 18:18:36 +0000

    Add some text that explains the game at the start

--- Test projects/run test copy.py
+++ Test projects/run test copy.py
@@ -1,83 +1,23 @@
-import random
+#Random module for randomly accepting the values
+# ‘X’ indicates the ships hit
+# ‘-‘ indicates the hits missed
+from random import randint
 
-class Battleship: 
-    
-    #Global variables for starting up game.
-    
-    def __init__(self): 
-        self.user_board = []
-        self.computer_board = []
-        self.user_hits = 0
-        self.computer_hits = 0
-        
-          
+num = 8
 
-    def start(self):
+Hidden_Pattern=[['~'for x in range(num)] for y in range (num)]
+Guess_Pattern=[['~'for x in range(num)] for y in range (num)]
 
-        #Function to create both boards and place ships.
+let_to_num={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7}
 
-        self.user_board = self.create_boards()
-        self.computer_board = self.create_boards()
-        self.display_boards()
-        self.user_board  = self.place_ships()
-        self.computer_board = self.place_ships()
-
-        #self.guess_place = []
+def print_board(board):
+    print(' A B C D E F G H')
+    print(' ***************')
+    row_num=1
+    space = ''
+    for row in i(len(board)):
+        space+=" ".join(board[i])+'\n'
+    return(space)
 
 
-    def create_boards(self):
-
-        # Board layout to be used in both
-                
-        num = 8
-
-        board = [['~'for x in range(num)] for y in range (num)]
-        some_str = ''
-        for i in range (len(board)):
-            some_str+=" ".join(board[i])+'\n'
-        return(some_str)
-
-    
-    def place_ships(self):
-        for ships in range(6):
-            num_ships = 0
-            while num_ships < ships:
-                x = random.randint(0, 7)
-                y = random.randint(0, 7)
-                if self.board[x][y] == '~':
-                    self.board[x][y] = '@'
-                    num_ships += 1
-                
-        
-   
-    def guess_place(self, board):
-        int(input("Write a row number 0-7:\n"))
-        int(input("Write a column number 0-7:\n"))
-        if board[x][y] == '@':
-            print("Hit!")
-            self.user_hits += 1
-        else:
-            print("Miss!")
-        
-    
-
-    def display_boards(self):
-        print("\nWELCOME TO BATTLESHIP")
-        print("\nAn enemy fleet is spotted on the horizon.")
-        print("Try to sink all their ships before they sink your ships.\n")
-        print("Your board:")
-        print(self.user_board)
-        print("Computer board:")
-        print(self.computer_board)
-        print(self.guess_place)
-
-
-        """
-        self.place_ships(self.computer_board)
-        self.place_ships(self.user_board)
-        self.guess_place(self.computer_board)
-        """
-
-battleship = Battleship()
-# start the game
-battleship.start()
+print(print_board(Guess_Pattern))
--- run.py
+++ run.py
@@ -1,16 +1,25 @@
 from random import randint
+import time
 
 num = 8
 
 Hidden_Pattern=[['~'for x in range(num)] for y in range (num)]
 Guess_Pattern=[['~'for x in range(num)] for y in range (num)]
 
-def create_board():
-    
-    some_str = ''
-    for i in range (len(Guess_Pattern)):
-        some_str+=" ".join(Guess_Pattern[i])+'\n'
-    return(some_str)
 
-print(create_board())
+def create_board(board):
+   
+    space = ''
+    for i in range (len(board)):
+        space+=" ".join(board[i])+'\n'
+    return(space)
+
+
+print("\nWELCOME TO BATTLESHIP")
+print("\nAn enemy fleet is spotted on the horizon.")
+print("You have 10 missiles at your disposal.")
+print("Try to sink all their ships before it's too late.\n")
+time.sleep(5)
+print("\nCan you guess where on the board the 6 hidden ships are?")
+print(create_board(Guess_Pattern))
     
