21678fd7cbb25d5a823eb9ebc1473ce5c98e852b
ElisabethKAndersson <>
2023-07-22 18:50:21 +0000

    Add function that creates ships on hidden board. Test function by printing hidden board

--- Test projects/run test copy.py
+++ Test projects/run test copy.py
@@ -14,10 +14,39 @@ def print_board(board):
     print(' A B C D E F G H')
     print(' ***************')
     row_num=1
-    space = ''
-    for row in i(len(board)):
-        space+=" ".join(board[i])+'\n'
-    return(space)
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num +=1
+
+def get_ship_location():
+    #Enter the row number between 1 to 8
+    row=input('Please enter a ship row 1-8 ').upper()
+    while row not in '12345678':
+        print("Please enter a valid row ")
+        row=input('Please enter a ship row 1-8 ')
+    #Enter the Ship column from A TO H
+    column=input('Please enter a ship column A-H ').upper()
+    while column not in 'ABCDEFGH':
+        print("Please enter a valid column ")
+        column=input('Please enter a ship column A-H ')
+    return int(row)-1,let_to_num[column]
+
+#Function that creates the ships
+def create_ships(board):
+    for ship in range(5):
+        ship_r, ship_cl=randint(0,7), randint(0,7)
+        while board[ship_r][ship_cl] =='X':
+            ship_r, ship_cl = randint(0, 7), randint(0, 7)
+        board[ship_r][ship_cl] = 'X'
 
 
-print(print_board(Guess_Pattern))
+
+def count_hit_ships(board):
+    count=0
+    for row in board:
+        for column in row:
+            if column=='X':
+                count+=1
+    return count
+
+print_board(Guess_Pattern)
--- run.py
+++ run.py
@@ -1,14 +1,15 @@
+#Import random module for random placement of ships. Import time module to create delays.
 from random import randint
 import time
 
 num = 8
 
+#Hidden board for random ship placement. Guess board that shows places guesses.
 Hidden_Pattern=[['~'for x in range(num)] for y in range (num)]
 Guess_Pattern=[['~'for x in range(num)] for y in range (num)]
 
-
+#Creates a board layout with spaces between each 
 def create_board(board):
-   
     space = ''
     for i in range (len(board)):
         space+=" ".join(board[i])+'\n'
@@ -24,13 +25,22 @@ def guess_ship_location():
         print('That is not a valid column number')
         column = input("Write a column number 0-7:")
 
+def create_ships(board):
+    for ship in range(6):
+        ship_row, ship_col = randint(0,7), randint(0,7)
+        while board[ship_row][ship_col] =='X':
+            ship_row, ship_col = randint(0, 7), randint(0, 7)
+        board[ship_row][ship_col] = 'X'
+
+create_ships(Hidden_Pattern)
+print(create_board(Hidden_Pattern))
 
 print("\nWELCOME TO BATTLESHIP")
 print("\nAn enemy fleet is spotted on the horizon.")
-print("You have 10 missiles at your disposal.")
+print("You have 12 missiles at your disposal.")
 print("Try to sink all their ships before it's too late.\n")
 time.sleep(5)
 print("\nCan you guess where on the board the 6 hidden ships are?")
 print(create_board(Guess_Pattern))
 guess_ship_location()
-    
+
